3fde2ebbb4baa2720abab7aebebe8705220f63cf
Iraz Irfan <irazirfan@gmail.com>
2021-12-02 05:46:23 +0600

    todo, todolist temp init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 //Importing Components
 import Form from "./components/Form";
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
-      <Form />
+      <Form 
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
